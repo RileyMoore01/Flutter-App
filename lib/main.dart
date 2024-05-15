fe170e589c35f8f02dc94eeccbee1e375d8ba374
Riley Moore <xrileyxmoorex@outlook.com>
2024-05-15 17:43:41 -0500

    learning flutter basics

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main () {
-  runApp(MaterialApp(home: Text('hello world!')));
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.black]
+              )
+          ),
+          child: const Center(
+            child: Text('hello world!'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
